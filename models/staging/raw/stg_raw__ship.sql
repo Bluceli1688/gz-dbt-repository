ad64df06ec71f90537154132451712436d1d6adc
bluce li <paalring12@gmail.com>
2025-01-16 09:41:39 +0000

    1-15 commit

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -2,7 +2,7 @@ with
 
 source as (
 
-    select * from {{ source('gz_raw_data', 'raw_gz_ship') }}
+    select * from {{ source('raw', 'ship') }}
 
 ),
 
@@ -11,8 +11,9 @@ renamed as (
     select
         orders_id,
         shipping_fee,
+        shipping_fee_1,
         logcost,
-        CAST(ship_cost AS FLOAT64)
+        ship_cost
 
     from source
 
